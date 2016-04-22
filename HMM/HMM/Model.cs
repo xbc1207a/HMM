6a8c7f9e5f776e0cbaaab4b3add2f14e1ddf73b0
Lee <xbc1207a@gmail.com>
2016-04-22 14:00:35 +0800

    add a two dimension array and the constructor of the Model

--- HMM/HMM/Model.cs
+++ HMM/HMM/Model.cs
@@ -8,6 +8,19 @@ namespace HMM
 {
     class Model
     {
-        const int size = 0;
+        int size = 3;
+        double[,] transferMatrix = new double[3,3];
+
+        public Model()
+        {
+            for(int i = 0; i < 3; i = i + 1)
+            {
+                for(int j = 0; j < 3; j = j + 1)
+                {
+                    if (i == j) transferMatrix[i, j] = 0.34;
+                    else transferMatrix[i, j] = 0.33;
+                }
+            }
+        }
     }
 }
