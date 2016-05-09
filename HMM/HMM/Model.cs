48cf0d8f0b34fe5593334a477f929c250313541e
Lee <xbc1207a@gmail.com>
2016-05-09 20:28:59 +0800

    add a method to print the transfer matrix

--- HMM/HMM/Model.cs
+++ HMM/HMM/Model.cs
@@ -9,8 +9,8 @@ namespace HMM
     class Model
     {
         private int size = 3;
-        private double[,] transferMatrix = new double[3,3];
-        //private double[,] a = new double[3, 3];
+        //private double[,] transferMatrix = new double[3,3];
+        private double[,] a = new double[3, 3];
         private double[,] b = new double[3, 3];
         private double[] start = new double[3];
         // constructor
@@ -20,16 +20,27 @@ namespace HMM
             {
                 for(int j = 0; j < 3; j = j + 1)
                 {
-                    if (i == j) transferMatrix[i, j] = 0.34;
-                    else transferMatrix[i, j] = 0.33;
+                    if (i == j) a[i, j] = 0.34;
+                    else a[i, j] = 0.33;
                 }
             }
         }
         // methods
-        public double GetProbability(string s)
+        /*public double GetProbability(string s)
         {
             double p = 0;
             for()
+        }*/
+        public void PrintAMatrix()
+        {
+            for(int i = 0; i < this.size; i = i + 1)
+            {
+                for(int j = 0; j < this.size; j = j + 1)
+                {
+                    Console.Write("a["+i+","+"]="+a[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
         }
     }
 }
